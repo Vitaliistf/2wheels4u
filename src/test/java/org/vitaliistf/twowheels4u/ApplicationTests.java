f01a55c5639eab61f5e3816283be62910af85cb9
Vitalii Stefaniv <deosplayt@gmail.com>
2023-08-16 18:00:25 +0300

    Initial commit

---
+++ src/test/java/org/vitaliistf/twowheels4u/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vitaliistf.twowheels4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

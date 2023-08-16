bb62762e69d0f258c011c9c9ee260b923e70af5f
Vitalii Stefaniv <deosplayt@gmail.com>
2023-08-16 19:11:53 +0300

    Added Liquibase changelog files

---
+++ src/main/resources/db/changelog/changes/fill-users-table.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email, first_name, last_name, password, role) VALUES
+    ('manager@mail.com', 'Manager', 'Manager', '$2a$10$ziGUQbXotIGXDH1JDS2I6uDAwr75xTY3D3CqpMs2a7BPE0qXMZ1FK', 'MANAGER'),
+    ('user@mail.com', 'Vitalii', 'Stefaniv', '$2a$10$8qW76a3dE6nmAJKOtdyR9OnC8yKxPWG9xUTti5o07zU9srFDEYqzS', 'CUSTOMER');
